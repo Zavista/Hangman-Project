d47c15040432368081dc8cac6d38954a0a1d375b
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-04 20:48:33 -0400

    Set up App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { useState, useEffect } from "react";
+import wordsData from "./wordsList.json";
+
 const App = () => {
-  return <div>App</div>;
+  const words: string[] = wordsData;
+
+  const [wordToGuess, setWordGuess] = useState<string>("");
+  const [guessLetters, setGuessedLetters] = useState<string[]>([]);
+
+  useEffect(() => {
+    setWordGuess(words[Math.floor(Math.random() * words.length)]);
+  }, []);
+
+  return <div>{wordToGuess}</div>;
 };
 
 export default App;
