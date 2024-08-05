cd2fd085147aa54abc86b8a997aa79a0aad951af
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-04 20:58:37 -0400

    container setup

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,31 @@ const App = () => {
   const [wordToGuess, setWordGuess] = useState<string>("");
   const [guessLetters, setGuessedLetters] = useState<string[]>([]);
 
+  const containerStyle: React.CSSProperties = {
+    maxWidth: "800px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "2rem",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "auto",
+    backgroundColor: "lightblue",
+  };
+
+  const textStyle: React.CSSProperties = {
+    fontSize: "2rem",
+    textAlign: "center",
+  };
+
   useEffect(() => {
     setWordGuess(words[Math.floor(Math.random() * words.length)]);
   }, []);
 
-  return <div>{wordToGuess}</div>;
+  return (
+    <div style={containerStyle}>
+      <div style={textStyle}>{wordToGuess}</div>
+    </div>
+  );
 };
 
 export default App;
