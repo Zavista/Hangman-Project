6e6d801fcafdb2e89cb0806f175243bb33b3e0a6
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-04 21:48:10 -0400

    Created the gallow using styling

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,5 +1,48 @@
-const HangmanDrawing = () => {
-  return <div>HangmanDrawing</div>;
+import { CSSProperties } from "react";
+
+// Define the CSS properties as constants
+const containerStyle: CSSProperties = {
+  position: "relative",
+};
+
+const verticalPoleStyle: CSSProperties = {
+  height: "50px",
+  width: "10px",
+  background: "black",
+  position: "absolute",
+  top: 0,
+  right: 0,
+};
+
+const horizontalPoleStyle: CSSProperties = {
+  height: "10px",
+  width: "200px",
+  background: "black",
+  marginLeft: "120px",
+};
+
+const mainPoleStyle: CSSProperties = {
+  height: "400px",
+  width: "10px",
+  background: "black",
+  marginLeft: "120px",
+};
+
+const baseStyle: CSSProperties = {
+  height: "10px",
+  width: "250px",
+  background: "black",
+};
+
+const HangmanDrawing: React.FC = () => {
+  return (
+    <div style={containerStyle}>
+      <div style={verticalPoleStyle}></div>
+      <div style={horizontalPoleStyle}></div>
+      <div style={mainPoleStyle}></div>
+      <div style={baseStyle}></div>
+    </div>
+  );
 };
 
 export default HangmanDrawing;
