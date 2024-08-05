cf7c6e4160fa2bac76a82863c90c66f77a8e10a8
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-05 01:10:54 -0400

    added logic to reveal word if loser

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -12,18 +12,26 @@ const containerStyle: CSSProperties = {
 type HangmanWordProps = {
   wordToGuess: string;
   guessedLetters: string[];
+  reveal?: boolean;
 };
 
-const HangmanWord = ({ wordToGuess, guessedLetters }: HangmanWordProps) => {
+const HangmanWord = ({
+  wordToGuess,
+  guessedLetters,
+  reveal = false,
+}: HangmanWordProps) => {
   return (
     <div style={containerStyle}>
       {wordToGuess.split("").map((letter, index) => (
         <span key={index} style={{ borderBottom: ".1em solid black" }}>
           <span
             style={{
-              visibility: guessedLetters.includes(letter)
-                ? "visible"
-                : "hidden",
+              visibility:
+                guessedLetters.includes(letter) || reveal
+                  ? "visible"
+                  : "hidden",
+              color:
+                !guessedLetters.includes(letter) && reveal ? "red" : "black",
             }}
           >
             {letter}
