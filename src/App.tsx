2c113836c0866f803b678742ab640a20eaaaeb2c
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-04 21:47:27 -0400

    Moved styling out of component function

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,27 @@ import HangmanDrawing from "./components/HangmanDrawing";
 import HangmanWord from "./components/HangmanWord";
 import Keyboard from "./components/Keyboard";
 
+const containerStyle: React.CSSProperties = {
+  maxWidth: "800px",
+  display: "flex",
+  flexDirection: "column",
+  gap: "2rem",
+  justifyContent: "center",
+  alignItems: "center",
+  margin: "auto",
+  backgroundColor: "lightblue",
+};
+
+const textStyle: React.CSSProperties = {
+  fontSize: "2rem",
+  textAlign: "center",
+};
+
 const App = () => {
   const words: string[] = wordsData;
 
   const [wordToGuess, setWordGuess] = useState<string>("");
   const [guessLetters, setGuessedLetters] = useState<string[]>([]);
-
-  const containerStyle: React.CSSProperties = {
-    maxWidth: "800px",
-    display: "flex",
-    flexDirection: "column",
-    gap: "2rem",
-    justifyContent: "center",
-    alignItems: "center",
-    margin: "auto",
-    backgroundColor: "lightblue",
-  };
-
-  const textStyle: React.CSSProperties = {
-    fontSize: "2rem",
-    textAlign: "center",
-  };
 
   useEffect(() => {
     setWordGuess(words[Math.floor(Math.random() * words.length)]);
