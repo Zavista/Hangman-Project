3d93883f739af9a2c577985245c34d2eb0291cbd
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-04 23:23:55 -0400

    Keyboard setup

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,5 +1,51 @@
+import { CSSProperties } from "react";
+import styles from "../styles/Keyboard.module.css";
+
+const KEYS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+const containerStyle: CSSProperties = {
+  display: "grid",
+  gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+  gap: ".5rem",
+};
+
 const Keyboard = () => {
-  return <div>Keyboard</div>;
+  return (
+    <div style={containerStyle}>
+      {KEYS.map((key) => (
+        <button key={key} className={`${styles.btn}`}>
+          {key}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default Keyboard;
